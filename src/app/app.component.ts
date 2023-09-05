e7a0cb4e8fc11bb53f8e6943feb72035057f79b7
abdulRehamanS <abdulrehaman627@gmail.com>
2023-09-05 12:04:21 +0530

    integration with spring boot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularIntergrationWithSpringBoot';
+
+  msg: string = 'Welcome to angular ';
+
+  constructor(@Inject(HttpClient) private httpclient: HttpClient) {}
+
+  onclick() {
+    this.httpclient
+      .get('http://localhost:9090/', { responseType: 'text' })
+      .subscribe((res) => {
+        this.msg = res;
+      });
+  }
+
+  Wish() {
+    this.httpclient
+      .get('http://localhost:9090/wish', { responseType: 'text' })
+      .subscribe((respos) => {
+        this.msg = respos;
+      });
+  }
 }
